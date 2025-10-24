f76377c46094295eda1d2b7684f8265be11adfc8
Eddyfosso <eddyfosso@yahoo.com>
2025-10-24 13:16:36 -0600

    Update 1_Bio.py

--- streamlit_CS/pages/1_Bio.py
+++ streamlit_CS/pages/1_Bio.py
@@ -3,18 +3,18 @@ import streamlit as st
 st.title("👋 My Bio")
 
 # ---------- TODO: Replace with your own info ----------
-NAME = "Your Name"
-PROGRAM = "Your Program / Major / Role"
+NAME = "Eddy Fosso"
+PROGRAM = "CS39AE Data Visualization / Data Science and Machine Learning / Student"
 INTRO = (
-    "Write 2–3 sentences about yourself: what you’re studying/working on, "
-    "what excites you about data visualization or computing, etc."
+    "Iam student at Metropolitant University of Denver, "
+    "what excites me about data visualization or computing is the ability to turn raw data into actionable insights that solve real-world problems."
 )
 FUN_FACTS = [
-    "I love …",
-    "I’m learning …",
-    "I want to build …",
+    "I love pancakes",
+    "I’m learning python",
+    "I want to build website",
 ]
-PHOTO_PATH = "your_photo.jpg"  # Put a file in repo root or set a URL
+PHOTO_PATH = "https://1drv.ms/i/c/0c977a7ae1d9e64f/EU_m2eF6epcggAxXAQAAAAAB8CLIH8HCTTTFTBn66nvQkA?e=ZmgNRh"  # Put a file in repo root or set a URL
 
 # ---------- Layout ----------
 col1, col2 = st.columns([1, 2], vertical_alignment="center")
@@ -28,10 +28,9 @@ with col2:
     st.subheader(NAME)
     st.write(PROGRAM)
     st.write(INTRO)
-
 st.markdown("### Fun facts")
 for i, f in enumerate(FUN_FACTS, start=1):
-    st.write(f"- {f}")
+    st.write(f"- {f}"")
 
 st.divider()
 st.caption("Edit `pages/1_Bio.py` to customize this page.")
